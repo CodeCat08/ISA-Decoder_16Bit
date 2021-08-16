31257344732c985844686feda841de8e7285ca5d
David <27027506+CodeCat08@users.noreply.github.com>
2021-08-16 19:43:59 -0400

    Update ADCoperation.cs

--- ADCoperation.cs
+++ ADCoperation.cs
@@ -3,9 +3,6 @@
 // Project name:    ISA
 // Project description: Decoder for our awesome Detached-Toe 16-bit RISC ISA.
 // ------------------------------------------------------------------------------------------------------------------------
-// Creator's name and email: Harrison Lee Pollitte. pollitteh@etsu.edu. Edgar Bowlin III, bowline@etsu.edu., David Nelson nelsondk@etsu.edu , Michael Edwards edwardsmr@etsu.edu
-// Course Name: CSCI-4727 Computer Architecture
-// Course Section: 940
 // Creation Date: 03/31/2021
 // ------------------------------------------------------------------------------------------------------------------------
 namespace ISA_Decoder_16Bit {
